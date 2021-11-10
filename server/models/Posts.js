5f5782727844aed48d6e9f9392ed5990b6574e8c
Deku <zachdsamson@gmail.com>
2021-11-09 20:31:30 -0700

    post model defined

---
+++ server/models/Posts.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reactions');
+const dateFormat = require('../utils/dateFormat');
+
+const postSchema = new Schema(
+    {
+        postType: {
+            type: Boolean,
+            // true = PUSH false = PULL
+            required: true
+        },
+        // FE BE or Full-Stack
+        skillTag: {
+            type: String,
+            required: true
+        },
+        blurb: {
+            type: String,
+            required: true,
+            maxlength: 300
+        },
+        username: {
+            type: String, 
+            required: true
+        },
+        // is this push/pull complete? true = YES false = NO
+        resolved: {
+            type: Boolean,
+            default: false
+        },
+        reactions: [reactionSchema],
+        budget: {
+            type: Number
+        }
+
+    }
+);
+
+postSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Post = model('Post', postSchema);
+
+module.exports = Post;
