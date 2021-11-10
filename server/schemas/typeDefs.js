780a0a5496f28d40efc781433083fe2164bba803
Deku <zachdsamson@gmail.com>
2021-11-09 20:47:54 -0700

    typeDefs defined

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,30 @@
+// import the gql template function
+const { gql } = require('apollo-server-express');
 
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        posts: [Post]
+    }
+
+    type Post {
+        _id: ID
+        postType: Boolean
+        skillTag: String
+        blurb: String
+        username: String
+        resolved: Boolean
+        reactions: [Reaction]
+        budget: Int
+        createdAt: String
+    }
+
+    type Reaction {
+        _id: ID
+        reactionBody: String
+        createdAt: String
+        username: String
+    }
+`
