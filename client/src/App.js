7dd412960aaf95cfc8b5772ef86dfd82cdffba39
jhlanger <jonhenrylangerman@gmail.com>
2021-11-09 19:47:20 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import Home from './pages/Home';
+
+
+function App() {
+  return (
+    <div>
+        <Home></Home>
+    </div>
+)
+}
+
+export default App;
