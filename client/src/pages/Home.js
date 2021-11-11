ad2b11e5f65fc12bc52700bacf6554a602612959
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-11-10 20:12:12 -0700

    Add material ui dependencies

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 
 import React from 'react'
+import { Button } from '@mui/material';
 
 const Home = () => {
     return (
         <div>
             <h1>Dev-Wiki</h1>
+            <Button variant="contained">Hello World</Button>
         </div>
     )
 }
