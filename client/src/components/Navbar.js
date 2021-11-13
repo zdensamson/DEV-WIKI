1cd7f5abfb72ef73911b8a0bdfd0efc37ecfc031
jhlanger <jonhenrylangerman@gmail.com>
2021-11-13 13:49:23 -0700

    login/logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import './Navbar.css';
 import { IconContext } from 'react-icons';
+import Auth from '../utils/auth';
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
@@ -14,11 +15,15 @@ function Navbar() {
   return (
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
-        <div className='navbar'>
+
+        <div className='navbar usernameText'>
           <Link to='#' className='menu-bars'>
             <FaIcons.FaBars onClick={showSidebar} />
           </Link>
+          <p  style = {{color: "white"}}>Hello {Auth.getProfile().data.username}</p>
         </div>
+
+
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
           <ul className='nav-menu-items' onClick={showSidebar}>
             <li className='navbar-toggle'>
@@ -26,6 +31,7 @@ function Navbar() {
                 <AiIcons.AiOutlineClose />
               </Link>
             </li>
+            
             {SidebarData.map((item, index) => {
               return (
                 <li key={index} className={item.cName}>
