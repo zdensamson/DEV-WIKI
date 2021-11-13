d69bcb89e3984dda17e39175d9a735cbe232184b
jhlanger <jonhenrylangerman@gmail.com>
2021-11-13 12:19:34 -0700

    reactions component

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -17,6 +17,9 @@ import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
+
+import ReactionList from '../ReactionList/index';
+
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
@@ -34,7 +37,7 @@ export default function PostCard({ post }) {
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
-
+  console.log(post);
   return (
 
 
@@ -43,7 +46,7 @@ export default function PostCard({ post }) {
       <CardHeader
         avatar={
           <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            {/* {post} */}
+            {post.username.charAt(0)}
           </Avatar>
         }
         action={
@@ -51,19 +54,23 @@ export default function PostCard({ post }) {
             <MoreVertIcon />
           </IconButton>
         }
-        title="Shrimp and Chorizo Paella"
+        title={`${post.skillTag}  Help Wanted `}
         subheader="September 14, 2016"
       />
       {/* IMAGE GOES BELOW */}
       <CardMedia
         component="img"
-        // height="194"
-        // image="/static/images/cards/paella.jpg"
-        // alt="Paella dish"
+      // height="194"
+      // image="/static/images/cards/paella.jpg"
+      // alt="Paella dish"
       />
       <CardContent>
+
         <Typography variant="body2" color="text.secondary">
-         {post.blurb}
+          {post.username}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {post.blurb}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
@@ -83,34 +90,25 @@ export default function PostCard({ post }) {
         </ExpandMore>
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-        </CardContent>
+        {post.reactions.length ? (
+          <CardContent>
+
+         <ReactionList reactions = {post.reactions} ></ReactionList>
+
+
+
+
+            {/* <Typography paragraph>Method:</Typography>
+
+
+
+            <Typography paragraph>
+              p1
+            </Typography> */}
+          </CardContent>
+        ) : (<div> No Reactions </div>)}
       </Collapse>
+
     </Card>
 
 
