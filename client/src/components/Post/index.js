12ae774c288e8b33cf43a756b91547271426a080
Deku <zachdsamson@gmail.com>
2021-11-15 18:42:03 -0700

    conditinally render delete button for logged in user

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -12,13 +12,12 @@ import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { Link } from 'react-router-dom';
 
 import ReactionList from '../ReactionList/index';
+import Auth from '../../utils/auth';
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -31,7 +30,7 @@ const ExpandMore = styled((props) => {
   }),
 }));
 
-export default function PostCard( {post} ) {
+export default function PostCard({ post }) {
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
@@ -39,9 +38,6 @@ export default function PostCard( {post} ) {
   };
   // console.log(post);
   return (
-
-
-
     <Card sx={{ maxWidth: 345 }}>
       <Link to={`/post/${post._id}`}>
         <CardHeader
@@ -49,11 +45,6 @@ export default function PostCard( {post} ) {
             <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
               {post.username.charAt(0)}
             </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
           }
           title={`${post.skillTag}  Help Wanted `}
           subheader={`${post.createdAt}`}
@@ -76,12 +67,18 @@ export default function PostCard( {post} ) {
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
+        {Auth.loggedIn() ? (
+          Auth.getProfile().data.username === post.username ?
+            (
+              <IconButton aria-label="delete post">
+                <DeleteIcon />
+              </IconButton>
+            ) :
+            (<></>)
+        ) : (<></>)}
+
+
+
         <ExpandMore
           expand={expanded}
           onClick={handleExpandClick}
@@ -95,7 +92,7 @@ export default function PostCard( {post} ) {
         {post.reactions.length ? (
           <CardContent>
 
-         <ReactionList reactions = {post.reactions} ></ReactionList>
+            <ReactionList reactions={post.reactions} ></ReactionList>
 
 
 
