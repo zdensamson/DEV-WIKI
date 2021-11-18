8918698b29aaa20707ee8595fb60c47d20a773b1
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-11-18 12:22:21 -0700

    Contact floating form

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -10,10 +10,6 @@ export default function Contact() {
                         <label htmlFor="contactEmail" id="contactEmail">Email address</label>
                         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                     </div>
-                    {/* <div className="mb-3 form-check">
-                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-                    </div> */}
                     <div className="form-floating mb-3">
                         <textarea type="text" className="form-control" id="message"></textarea>
                         <label htmlFor="message">Message</label>
