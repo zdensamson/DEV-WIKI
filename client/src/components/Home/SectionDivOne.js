d87ffd4632af158d8148321b11bc968e0f8163b9
jhlanger <jonhenrylangerman@gmail.com>
2021-11-17 23:58:10 -0700

    added media quieries to homepage

--- client/src/components/Home/SectionDivOne.js
+++ client/src/components/Home/SectionDivOne.js
@@ -7,10 +7,10 @@ const SectionDivOne = () => {
             <div className="container">
                 <h2> &lt;section&gt;</h2>
                 <div className="d-flex flex-wrap">
-                    <div className="col-6 ">
+                    <div className=" d-flex flex-wrap">
                         <img src={dogCode} alt="dog code image"></img>
                     </div>
-                    <div className="col-6 my-auto">
+                    <div className="my-auto col-6">
                         <h1>SectionDivCorgi</h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
                     </div>
--- client/src/components/Home/SectionDivTwo.js
+++ client/src/components/Home/SectionDivTwo.js
@@ -11,7 +11,7 @@ const SectionDivTwo = () => {
                         <h1>SectionDivTwo</h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
                     </div>
-                    <div className="col-6 ">
+                    <div className=" ">
                         <img src={girlCode} alt="dog code image"></img>
                     </div>
 
