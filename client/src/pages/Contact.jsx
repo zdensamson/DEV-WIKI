7c32445243f254a1a1e423783aefd47ee7ba5ba1
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-11-18 16:25:44 -0700

    Change layout

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -6,12 +6,12 @@ export default function Contact() {
         <div>
             <div className="container">
                 <form>
-                    <div className="form-floating mb-3 mt-3">
+                    <div className="form-floating mb-3 mt-3 col-sm-5 d-flex justify-center flex-wrap">
                         <input type="email" className="form-control" id="contactEmail" aria-describedby="emailHelp" />
                         <label htmlFor="contactEmail" id="contactEmail">Email address</label>
                         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                     </div>
-                    <div className="form-floating mb-3">
+                    <div className="form-floating mb-3 col-sm-5">
                         <textarea type="text" className="form-control" id="message"></textarea>
                         <label htmlFor="message">Message</label>
                     </div>
