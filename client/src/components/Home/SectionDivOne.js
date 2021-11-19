6b07a3476afdc2bc6dc6107f88b6b2cdad43f396
jhlanger <jonhenrylangerman@gmail.com>
2021-11-18 18:07:13 -0700

    home page media quieries

--- client/src/components/Home/SectionDivOne.js
+++ client/src/components/Home/SectionDivOne.js
@@ -10,7 +10,7 @@ const SectionDivOne = () => {
                     <div className=" d-flex flex-wrap">
                         <img src={dogCode} alt="dog code"></img>
                     </div>
-                    <div className="my-auto col-6">
+                    <div className="my-auto col-lg-6 col-xs-12">
                         <h1>SectionDivCorgi</h1>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
                     </div>
